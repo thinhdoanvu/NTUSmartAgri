1cc2487eb8d6ce4a3b54f95455fa18ca7d4a8bef
THINH DOAN VU <thinhdv@ntu.edu.vn>
2022-08-16 13:10:35 +0700

    Update Autorun_4web.sh

--- R-shiny/Scripts/Autorun_4web.sh
+++ R-shiny/Scripts/Autorun_4web.sh
@@ -100,7 +100,6 @@ echo -e "delete file output.dat"
 end=$(($number - 1))
 echo 2 $end | sed 's/ /,/g' | awk '{print "sed -i "$0"d www/output.dat"}' > del.sh
 echo 2 $end | sed 's/ /,/g' | awk '{print "sed -i "$0"d www/request.req"}' >> del.sh
-echo 2 $end | sed 's/ /,/g' | awk '{print "sed -i "$0"d www/control.txt"}' >> del.sh
 bash del.sh
 echo -e "Not enough data output.dat lines"
 fi
